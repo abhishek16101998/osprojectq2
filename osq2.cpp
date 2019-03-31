5055c8fcc2515e6026c67661b64cb8e0f8e3d12a
abhishek16101998 <32011708+abhishek16101998@users.noreply.github.com>
2019-03-31 07:07:09 +0530

    Add files via upload
    First Fit

--- osq2.cpp
+++ osq2.cpp
@@ -1,9 +1,30 @@
 #include<bits/stdc++.h>
+#include<stdio.h>
 using namespace std;
+
+struct node  
+{ 
+  int data; 
+  struct node *next; 
+}; 
 
 int main()
 {
+	struct node *prev,*head,*p;
 	int mem[5]={100,500,200,300,600}, process[4]={212,417,112,426};
 	
+	int i,j;
+	for(i=0;i<4;i++)
+	{
+		for(j=0;j<5;j++)
+		{
+			if(mem[j]!=0 && process[i]<=mem[j])
+			{
+				cout<<"Process Requiring Memory "<<process[i]<<" is allocated memory with size "<<mem[j]<<endl;
+				mem[j]=0;
+				j=5;
+			}
+		}
+	}
 	
 }
